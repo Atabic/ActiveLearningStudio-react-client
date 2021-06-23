375ec564f3fd5736b3cb184c278fda10948ad8f7
Muhammad <muhammadqamar111@gmail.com>
2021-06-23 11:58:49 +0500

    footer model box added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from 'react-responsive-modal';
+
 import logo from 'assets/images/studio_black_transparent.png';
+import headerLOGO from 'assets/images/openeducation_logo.png';
 import cc from 'assets/images/cc.png';
 import by from 'assets/images/by.png';
+import 'react-responsive-modal/styles.css';
 
 function Footer() {
+  const [open, setOpen] = useState(false);
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
   return (
     <div className="footer-img-section">
       <footer className="footer-all">
-        <a target="_blank" href="https://www.curriki.org/terms-of-service/" rel="noopener noreferrer">
+        <a style={{ cursor: 'pointer' }} target="_blank" onClick={onOpenModal} rel="noopener noreferrer">
           Terms of Service
         </a>
-        <a target="_blank" href="https://www.curriki.org/privacy-policy/" rel="noopener noreferrer">
+        <a style={{ cursor: 'pointer' }} target="_blank" onClick={onOpenModal} rel="noopener noreferrer">
           Privacy Policy
         </a>
-        <a target="_blank" href="https://support.curriki.org/" rel="noopener noreferrer">
+        <a style={{ cursor: 'pointer' }} target="_blank" href="https://support.curriki.org/" rel="noopener noreferrer">
           Help & Support
         </a>
       </footer>
@@ -27,6 +34,16 @@ function Footer() {
         <span>Powered by</span>
         <img src={logo} alt="" />
       </div>
+      <Modal open={open} onClose={onCloseModal} center classNames="footer-model-react">
+        <div className="img-logo"><img src={headerLOGO} alt="" /></div>
+        <div className="coming-soon">
+          Coming Soon.
+          <p>
+            Learn more about CurrikiStudio:
+            <a target="_blank" rel="noreferrer" href="https://www.curriki.org/">Click Here</a>
+          </p>
+        </div>
+      </Modal>
     </div>
   );
 }
